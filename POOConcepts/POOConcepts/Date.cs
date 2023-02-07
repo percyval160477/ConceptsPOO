55da3cb1ccf6890356688bd27df43f6afdccf296
Percy <pegarciap160477@gmail.com>
2023-02-07 07:31:21 -0500

    Finish date class

---
+++ POOConcepts/POOConcepts/Date.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOConcepts
+{
+    public class Date
+    {
+        //atributo privado
+        private int _year;
+        private int _month;
+        private int _day;
+
+        //Constructor
+        public Date(int year, int month,int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year,month, day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+            throw new DayException("Invalid day");
+        }
+    
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            //if (year % 4 == 0)
+            //{
+            //    if (year % 100 == 0)
+            //    {
+            //        if (year % 400 == 0)
+            //        {
+            //            return true;
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        return true;
+            //    }
+            //}
+        }
+
+        //Metodo
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            throw new MonthException("Invalid month");
+        }
+
+        //Sobreescribir el ToString
+        public override string ToString()
+        {
+            //interpolacion de string
+            return $"{_year}/{_month:00}/{_day:00}";
+        }
+    }
+}
---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOConcepts;
+
+Console.WriteLine("POOConcepts");
+Console.WriteLine("=============");
+//Instanciar un objeto de la clase year
+//Date date1 = new Date(2022,2,11);
+//Console.WriteLine(date1);
+//CTRL + K, CTRL + S
+try
+{
+	Console.WriteLine(new Date(2022, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
